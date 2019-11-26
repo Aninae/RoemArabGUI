205f1c15029b5e136e0524be799465c1b7e69d69
Anina <Anina@LAPTOP-RR73I1G3>
2019-11-26 19:34:16 +0100

    Hier irgendwas eingeben was du geändert hast

--- RoemArabGUI/Form1.cs
+++ RoemArabGUI/Form1.cs
@@ -16,5 +16,10 @@ namespace RoemArabGUI
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Hallo";
+        }
     }
 }
